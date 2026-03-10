c8933e63333ce123a992c4fdcd3d714ea2326a8d
montasirmoyen <montasirmoyen@gmail.com>
2026-03-09 22:54:41 -0400

    Simplify MongoDB connection by removing deprecated options

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectMongo() {
-  await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(process.env.MONGODB_URI);
 }
 
 module.exports = { connectMongo };
